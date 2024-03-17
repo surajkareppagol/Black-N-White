17e43e01963ef2ee914f172444f2986709e72eef
surajkareppagol <surajkareppagol.dev@gmail.com>
2024-03-17 19:12:09 +0530

    🌱 Add index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("Welcome To Black-N-White");
+
+const black = "#2C2C34";
+const white = "#ECFEE8";
+
+const main = document.body;
+
+main.setAttribute(
+  "style",
+  `background-color: ${black} !important;color: ${white} !important;`
+);
